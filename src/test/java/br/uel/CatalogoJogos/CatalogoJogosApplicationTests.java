0ecc07a155e5d1f7301c989085d6d98922786223
TayEleuterio <tay_eleuterio@hotmail.com>
2026-09-03 20:50:36 -0300

    Initial commit

---
+++ src/test/java/br/uel/CatalogoJogos/CatalogoJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uel.CatalogoJogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
